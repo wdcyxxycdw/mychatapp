45d4d0a610b047f05e82ed129bdcfbf7b9c8cb08
wdcyx <3207694306@qq.com>
2024-02-17 00:38:50 +0900

    2

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import React, { Component } from "react";
+import { connect, sendMsg } from "./api";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    connect();
+  }
+
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.send}>Hit</button>
+      </div>
+    );
+  }
+}
+
+export default App;
