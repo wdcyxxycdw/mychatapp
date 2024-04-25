5eed86e95cd4b12f5bbb09574576e879ba352fab
apple <3207694306@qq.com>
2024-04-25 20:57:59 +0900

    commit 2024-04-25

--- backend/main.go
+++ backend/main.go
@@ -7,23 +7,30 @@ import (
 	"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
 )
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Websocket Endpoint Hit")
 
 	// upgrade this connection to a WebSocket
 	// connection
-	ws, err := websocket.Upgrade(w, r)
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
 		fmt.Fprintf(w, "%+V\n", err)
-		go websocket.Writer(ws)
-		websocket.Reader(ws)
 	}
+	client:=&websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	pool.Register<-client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/ws", serveWs)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
+	pool:=websocket.NewPool()
+	go pool.Start()
+
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request){
+		serveWs(pool, w, r)
 	})
 }
 
